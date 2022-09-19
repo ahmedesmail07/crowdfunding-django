22022637770bb14f8547bde0a8be3a55f3310d39
Ahmed Esmail <ahmedismail332211@gmail.com>
2022-09-19 12:57:07 +0200

    Updating email address - 100% Works

--- crowdFunding/crowdFunding/settings.py
+++ crowdFunding/crowdFunding/settings.py
@@ -164,9 +164,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'ahmedismail332211@gmail.com'
-EMAIL_HOST_USER = 'ahmedismail332211@gmail.com'
-EMAIL_HOST_PASSWORD = 'jgpdqdhskhhgtwqm' # Password of the host mail
+EMAIL_FROM = 'pythontestmanagement@gmail.com'
+EMAIL_HOST_USER = 'pythontestmanagement@gmail.com'
+EMAIL_HOST_PASSWORD = 'tqsjrsahrkcanilt'
+ # Password of the host mail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
